40cd137131f79a81a6502dc0ae84c9f6e5e3b131
Gastón Estévez <gaston@digitalhouse.edu.ar>
2022-03-03 21:18:09 -0300

    Added hooks to Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,63 +1,42 @@
-import React, { Component } from "react"
+import React from "react"
 import { Link } from "react-router-dom"
-import './Navbar.css'
+import "./Navbar.css"
 import { logoutUser } from "../../redux/reducers/userReducer"
-import { connect } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 
-class Navbar extends Component {
+const Navbar = () => {
+    const { token, firstName } = useSelector((state) => state.userReducer)
+    const dispatch = useDispatch()
 
-    constructor(props){
-        super(props)
-        this.handleLogout = this.handleLogout.bind(this)
-    }
-
-    handleLogout(){
-        this.props.logout()
-    }
-
-    render() {
-        return (
-            <>
-                <div className="navbar-container">
-                    <ul className="navbar-list">
-                        <li className="navbar-item">
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li className="navbar-item">
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li className="navbar-item">
-                            <Link to="/contact">Contact</Link>
-                        </li>
-                        <li className="navbar-item">
-                            {
-                                this.props.userReducer.token ? (
-                                    <>
-                                        <p>Bienvenid@ {this.props.userReducer.firstName}</p>
-                                        <button onClick={this.handleLogout}>Logout</button>
-                                    </>
-                                ) : (
-                                    <Link to="/login">Login</Link>
-                                )
-                            }
-                        </li>
-                    </ul>
-                </div>
-            </>
-        )
-    }
+    return (
+        <>
+            <div className="navbar-container">
+                <ul className="navbar-list">
+                    <li className="navbar-item">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li className="navbar-item">
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                    <li className="navbar-item">
+                        {token ? (
+                            <>
+                                <p>Bienvenid@ {firstName}</p>
+                                <button onClick={() => dispatch(logoutUser())}>
+                                    Logout
+                                </button>
+                            </>
+                        ) : (
+                            <Link to="/login">Login</Link>
+                        )}
+                    </li>
+                </ul>
+            </div>
+        </>
+    )
 }
 
-const mapStateToProps = (state) => {
-    return {
-        userReducer: state.userReducer
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        logout: () => dispatch(logoutUser()),
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
+export default Navbar
